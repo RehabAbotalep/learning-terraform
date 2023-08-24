eb4c39e8cde7a78090753ecd1ee55c5a23633957
Rehab Abotalep <rehab.abotalep@gmail.com>
2023-08-24 20:46:34 +0100

    01-resource-group

---
+++ 01-resource-group/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.26.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "myFirstResourceGroup"
+  location = "centralus"
+}
