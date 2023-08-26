e863903b0c6473f49ca4f0e7f0d34e42ab26afa2
Rehab Abotalep <rehab.abotalep@gmail.com>
2023-08-26 16:57:40 +0100

    Overriding variables with .tfvars

---
+++ 02-app-service/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name     = "AppServiceRG"
+resource_group_location = "West Europe"
+app_service_plan_name   = "example-appserviceplan"
+app_service_name        = "terraform-webapp-lab-01"
--- 02-app-service/variables.tf
+++ 02-app-service/variables.tf
@@ -1,24 +1,20 @@
 variable "resource_group_name" {
-  default     = "AppServiceRG"
   type        = string
   description = "The resource group name"
 
 }
 
 variable "resource_group_location" {
-  default     = "West Europe"
   type        = string
   description = "The resource group loaction"
 }
 
 variable "app_service_plan_name" {
-  default     = "example-appserviceplan"
   type        = string
   description = "The app service paln name"
 }
 
 variable "app_service_name" {
-  default     = "terraform-webapp-lab-01"
   type        = string
   description = "The app service name"
 }
