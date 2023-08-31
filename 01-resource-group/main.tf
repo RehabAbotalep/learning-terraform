aada377ca8d9395136ceb3d51a7e74a60ae7050c
Rehab Abotalep <rehab.abotalep@gmail.com>
2023-08-31 07:57:36 +0100

    Amend resource group to use azuerm provider latest version

--- 01-resource-group/main.tf
+++ 01-resource-group/main.tf
@@ -1,17 +1,4 @@
-provider "azurerm" {
-  features {}
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.26.0"
-    }
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "myFirstResourceGroup"
-  location = "centralus"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
---
+++ 01-resource-group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name in Azure"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Resource location in Azure"
+}
