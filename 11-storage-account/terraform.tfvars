c9b6cdc2b0821b79f0a1ad865c5d631695008d06
Rehab Abotalep <rehab.abotalep@gmail.com>
2023-09-02 11:43:56 +0100

    Deploy Storage Account, Container, and Blob

---
+++ 11-storage-account/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name     = "StorageRG"
+resource_group_location = "West Europe"
+storage_account_name    = "storageacc0300"
+storage_container_name  = "my-files"
---
+++ 11-storage-account/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name in Azure"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Resource location in Azure"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account name in Azure"
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "Storage Container name in Azure"
+}
+
